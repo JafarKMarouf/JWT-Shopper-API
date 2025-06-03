400003a163fef40102bee1acda3f3ac7a1f4db77
JafarKMarouf <jafar.k.marouf@gmail.com>
2025-06-03 12:34:11 +0300

    set up Spring Boot base project with dependencies

---
+++ src/main/java/com/jafarmarouf/jwtshopper/JwtShopperApplication.java
@@ -0,0 +1,13 @@
+package com.jafarmarouf.jwtshopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtShopperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtShopperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jafarmarouf/jwtshopper/JwtShopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jafarmarouf.jwtshopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtShopperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
